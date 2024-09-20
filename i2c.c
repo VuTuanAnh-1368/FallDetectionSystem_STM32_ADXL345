d05fa1bd8fe0cbff0f415aa7e83ec8bc7ecf4860
Tuananh <vutuananhqn1368@gmail.com>
2024-09-20 10:42:55 +0700

    edit name --TA

--- i2c.c
+++ i2c.c
@@ -1,6 +1,6 @@
 #include "i2c.h"
 
-Status i2c_init(uint8_t i2c, unsigned short speed_mode){
+Status i2c_initTA(uint8_t i2c, unsigned short speed_mode){
 		RCC->APB2ENR  |= 1;
 		if(i2c == I2C_1){
 			RCC->APB1ENR |= (1 << 21);
@@ -25,7 +25,7 @@ Status i2c_init(uint8_t i2c, unsigned short speed_mode){
 
 }
 
-Status i2c_start(uint8_t i2c){
+Status i2c_startTA(uint8_t i2c){
 		if(i2c == I2C_1){
 			I2C1->CR1 |= 0x100;
 			while (!(I2C1->SR1 & (1 << 0))){};
@@ -37,7 +37,7 @@ Status i2c_start(uint8_t i2c){
 		return Success;
 }
 
-Status i2c_add(uint8_t i2c, char address, char RW){
+Status i2c_addTA(uint8_t i2c, char address, char RW){
 		volatile int tmp;
 
 		if(i2c == I2C_1){
