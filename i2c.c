ef567f60b24e2c5a20e73238be94f3b09edb2187
cong <thanhcong14pr@gmail.com>
2024-09-20 10:48:40 +0700

    fix i2c.c on cong1

--- i2c.c
+++ i2c.c
@@ -1,6 +1,6 @@
 #include "i2c.h"
 
-Status i2c_init(uint8_t i2c, unsigned short speed_mode){
+Status i2c_init1(uint8_t i2c, unsigned short speed_mode){
 		RCC->APB2ENR  |= 1;
 		if(i2c == I2C_1){
 			RCC->APB1ENR |= (1 << 21);
